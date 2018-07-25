ab10b141d53f19a3a4056733f07769950da6d990
Mike Brown <brownwm@us.ibm.com>
2018-07-25 12:45:31 -0500

    switches from config.digest to target.digest
    Signed-off-by: Mike Brown <brownwm@us.ibm.com>

--- pkg/server/helpers.go
+++ pkg/server/helpers.go
@@ -251,11 +251,7 @@ func (c *criService) localResolve(ctx context.Context, refOrID string) (*imagest
 			if err != nil {
 				return ""
 			}
-			desc, err := image.Config(ctx)
-			if err != nil {
-				return ""
-			}
-			return desc.Digest.String()
+			return image.Target().Digest.String()
 		}(refOrID)
 	}
 
@@ -343,7 +339,6 @@ func getImageInfo(ctx context.Context, image containerd.Image) (*imageInfo, erro
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get image config descriptor")
 	}
-	id := desc.Digest.String()
 
 	rb, err := content.ReadBlob(ctx, image.ContentStore(), desc)
 	if err != nil {
@@ -353,6 +348,8 @@ func getImageInfo(ctx context.Context, image containerd.Image) (*imageInfo, erro
 	if err := json.Unmarshal(rb, &ociimage); err != nil {
 		return nil, errors.Wrapf(err, "failed to unmarshal image config %s", rb)
 	}
+
+	id := image.Target().Digest.String()
 
 	return &imageInfo{
 		id:        id,
